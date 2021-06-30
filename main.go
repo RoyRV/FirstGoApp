5a790fba8f0bb997c859041989df59669f5b7847
Roy <roy.rojval@gmail.com>
2021-06-30 17:11:45 -0500

    delcaracion de variables

--- main.go
+++ main.go
@@ -8,5 +8,14 @@ puedo ejecutar este codigo de dos maneras
 -  go run *NombreArchivo*.go
 */
 func main() {
-	fmt.Println("sammy")
+	//declaracion tipo 1
+	var texto string
+	texto = "gaaa1"
+	fmt.Println(texto)
+	//declaracion tipo 2
+	var texto2 string = "gaaaaaa2"
+	fmt.Println(texto2)
+	//declaracion tipo 3
+	texto3 := "gaaaaa3"
+	fmt.Println(texto3)
 }
