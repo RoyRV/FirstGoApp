c905bf06423381142439a15c0d90d40f2d9f262f
Roy <roy.rojval@gmail.com>
2021-06-30 16:56:17 -0500

    segundo commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module FirstGoApp
+//para crear este doc , puse en el cmd 
+//go mod init FirstGoApp
+
+go 1.16
--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+/*
+puedo ejecutar este codigo de dos maneras
+-  go run FirstGoApp (el nombre del modulo)
+-  go run *NombreArchivo*.go
+*/
 func main() {
 	fmt.Println("sammy")
 }
